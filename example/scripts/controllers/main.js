dc787f75f2fced416c3f6180ba0abbbd2cfc3581
Rusty Green <rustymgreen+github@gmail.com>
2013-08-04 20:29:20 -0400

    Added example code to the example page.

--- example/scripts/controllers/main.js
+++ example/scripts/controllers/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
 angular.module('flickrApp')
-  .controller('MainCtrl', function ($scope, flickrLoading) {
-    $scope.flickrLoading = flickrLoading;
+  .controller('MainCtrl', function ($scope) {
   });
---
+++ modules/directives/photoset/photosetPanel.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('flickr.directives')
+  .directive('photosetPanel', function (flickrLoading) {
+    return {
+      scope: {
+        panelTitle: '@'
+      },
+      restrict: 'EA',
+      replace: true,
+      template: '<div class="panel">' +
+        '<div class="panel-heading">' +
+        '<div class="panel-title">{{panelTitle}} <loading-balls class="pull-right"></loading-balls></div>' +
+        '</div>' +
+
+        '<photoset-list list-class="breadcrumb" set="photoset"></photoset-list>' +
+
+        '<div class="row">' +
+        '<div class="col-lg-3">' +
+        '<photoset-thumbnails ng-show="photoset" thumbnail-class="img-thumbnail"' +
+        'photo="photo" set-id="{{photoset.id}}">' +
+        '</photoset-thumbnails>' +
+        '</div>' +
+
+        '<div class="col-lg-9">' +
+        '<photoset-slideshow photo="photo"></photoset-slideshow>' +
+        '</div>' +
+        '</div>' +
+        '</div>',
+
+      link: function (scope) {
+        scope.flickrLoading = flickrLoading;
+      }
+    };
+  });
