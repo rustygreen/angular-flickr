96fb1f3692607d14ffce4940de240df586f41769
tobi-or-not-tobi <tobiasouwejan@gmail.com>
2015-06-06 08:50:12 +0200

    Add secure api address to source files rather than distilled version only.

--- modules/services/PhotoSet.js
+++ modules/services/PhotoSet.js
@@ -3,7 +3,7 @@
 angular.module('flickr.services')
   .factory('Photoset', function ($resource, flickrConfig) {
     var config = angular.extend({
-      url: 'http://api.flickr.com/services/rest/',
+      url: 'https://api.flickr.com/services/rest/',
       params: {
         common: {
           format: 'json',     // Optional.
